a5d805fbdf277413130c907dd7f89d2680a37a72
mkcodelab <michal.kulawik@gmail.com>
2024-05-27 11:01:26 +0200

    sequencer wip

--- src/app/components/sequencer/sequencer.component.ts
+++ src/app/components/sequencer/sequencer.component.ts
@@ -1,16 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { SequencerService } from '../../services/sequencer.service';
+import { BeatButton, SequencerService } from '../../services/sequencer.service';
 import { NgClass } from '@angular/common';
-
-export class BeatButton {
-  constructor(public id: number) {}
-  isActive = false;
-}
-
-// export class InstrumentButton {
-//   constructor(public id: number) {}
-//   isActive = false;
-// }
 
 @Component({
   standalone: true,
@@ -20,45 +10,20 @@ export class BeatButton {
 })
 export class SequencerComponent {
   sequencerSvc = inject(SequencerService);
-  // array of some samples
-  //   instruments = ['primary', 'secondary', 'drum1', 'drum2'];
-  //   take synths from sequencerService
+
   instruments = this.sequencerSvc.synths;
 
-  beats: BeatButton[] = [];
-
-  instrumentButtons: BeatButton[][] = [];
-
-  isPlaying = false;
-
-  constructor() {
-    this.initButtons();
-  }
-
-  ngOnInit() {
-    console.log(this.sequencerSvc.transport);
-  }
+  instrumentButtons = this.sequencerSvc.instrumentButtons;
 
   sequencerToggle() {
-    this.isPlaying = !this.isPlaying;
-    this.isPlaying ? this.sequencerSvc.start() : this.sequencerSvc.stop();
-  }
-
-  initButtons() {
-    for (let instrument of this.instruments) {
-      const beatButtonArr = [];
-
-      for (let i = 0; i < 8; i++) {
-        beatButtonArr.push(new BeatButton(i));
-      }
-
-      this.instrumentButtons.push(beatButtonArr);
-    }
-
-    console.log(this.instrumentButtons);
+    this.sequencerSvc.sequencerToggle();
   }
 
   toggleActiveBeat(beatBtn: BeatButton) {
-    beatBtn.isActive = !beatBtn.isActive;
+    this.sequencerSvc.toggleActiveBeat(beatBtn);
+  }
+
+  get isPlaying(): boolean {
+    return this.sequencerSvc.isPlaying;
   }
 }
--- src/app/services/sequencer.service.ts
+++ src/app/services/sequencer.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import * as Tone from 'tone';
+import { Time } from 'tone/build/esm/core/type/Units';
+
+export class BeatButton {
+  constructor(public id: number) {}
+  isActive = false;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -9,19 +15,29 @@ export class SequencerService {
 
   constructor() {
     this.synths.forEach((synth) => synth.connect(this.gain));
-    //   this.transport.scheduleRepeat()
 
     this.gain.toDestination();
+    this.initButtons();
+    this.setBpm(120);
+    this.transport.scheduleRepeat((time) => {
+      this.repeat(time);
+    }, '8n');
   }
 
+  numberOfBeats = 8;
+
+  currentBeat = 0;
+
+  isPlaying = false;
+
+  instrumentButtons: BeatButton[][] = [];
+
+  //   change synths for some samples
   synths = [
     new Tone.Synth({ oscillator: { type: 'square' } }),
     new Tone.Synth({ oscillator: { type: 'triangle' } }),
     new Tone.Synth({ oscillator: { type: 'sawtooth' } }),
   ];
-
-  //   seq = Tone.Sequence
-  //   sequence = new Tone.Sequence()
 
   gain = new Tone.Gain(0.5);
 
@@ -34,5 +50,40 @@ export class SequencerService {
 
   setBpm(bpm: number) {
     this.transport.set({ bpm: bpm });
+  }
+
+  toggleActiveBeat(beatBtn: BeatButton) {
+    beatBtn.isActive = !beatBtn.isActive;
+  }
+
+  initButtons() {
+    for (let instrument of this.synths) {
+      const beatButtonArr = [];
+
+      for (let i = 0; i < this.numberOfBeats; i++) {
+        beatButtonArr.push(new BeatButton(i));
+      }
+
+      this.instrumentButtons.push(beatButtonArr);
+    }
+  }
+
+  sequencerToggle() {
+    this.isPlaying = !this.isPlaying;
+    this.isPlaying ? this.start() : this.stop();
+  }
+  repeat(time: Time) {
+    this.instrumentButtons.forEach((row, index) => {
+      let synth = this.synths[index];
+
+      let button = row[this.currentBeat];
+
+      if (button.isActive) {
+        // synth.triggerAttackRelease(note.note, '8n', time)
+        synth.triggerAttackRelease('C4', '8n', time);
+      }
+    });
+
+    this.currentBeat = (this.currentBeat + 1) % this.numberOfBeats;
   }
 }
