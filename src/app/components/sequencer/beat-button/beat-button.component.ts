bbc221572c8256cfe1ca165a44a9ad122f5f271d
mkcodelab <michal.kulawik@gmail.com>
2024-06-12 12:09:45 +0200

    radial note menu added

--- src/app/components/sequencer/beat-button/beat-button.component.ts
+++ src/app/components/sequencer/beat-button/beat-button.component.ts
@@ -9,16 +9,28 @@ import {
 import { InstrumentButton } from '../../../services/sequencer.service';
 import { ButtonNotes, NOTES } from '../../../shared/consts';
 import { ClickOutsideDirective } from '../../../directives/clickOutside.directive';
+import { RadialMenuComponent } from '../../radial-menu/radial-menu.component';
 
 @Component({
   standalone: true,
   selector: 'beat-button',
   templateUrl: './beat-button.component.html',
-  imports: [NgClass, ClickOutsideDirective],
+  imports: [NgClass, ClickOutsideDirective, RadialMenuComponent],
   styles: `
    .instrument-btn-active {
         background: hsl(120, 100%, 85%);
         box-shadow: 0 0 10px 5px hsl(120, 100%, 50%);
+    }
+
+    // todo: opacity animation on opening menu
+    .note-menu-hidden {
+        opacity: 0;
+        transition: opacity .2s ease;
+
+    }
+
+    .note-menu-show {
+        opacity: 1;
     }
   `,
 })
@@ -32,6 +44,8 @@ export class BeatButtonComponent {
   notesMenuOpen = false;
 
   notesOptions = NOTES;
+
+  octaves = Array.from(Array(6), (x, i) => i + 1);
 
   //   right click to open note dropdown menu
   //   emit event first, to close all opened menus from parent level, then open one that we clicked on.
@@ -60,7 +74,7 @@ export class BeatButtonComponent {
     this.instrumentButton.selectNote(value as ButtonNotes);
   }
 
-  selectOctave(value: string) {
-    this.instrumentButton.selectOctave(+value);
+  selectOctave(value: number) {
+    this.instrumentButton.selectOctave(value);
   }
 }
